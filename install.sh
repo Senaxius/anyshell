e8debacaa857bb0b281d9f7c25b9dcba5f735c97
Senaxius <alexander.minor004@gmail.com>
2021-10-05 01:20:15 +0200

    bug fix and new installer

--- install.sh
+++ install.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+Server_user=$(sed -n 5p ./anyshell)
+Server_user=$(echo $Server_user | sed -n 's/^.*=//p')
+Server_dns=$(sed -n 2p ./anyshell)
+Server_dns=$(echo $Server_dns | sed -n 's/^.*=//p')
+
 cat ./.anyshell/asci.txt
 echo -e "\n\nWelcome to the official anyshell Installer! :)"
 echo ""
@@ -40,9 +45,12 @@ if [ "$answer" == "yes" ]; then
     read -p "(yes | no): " answer2
     if [ "$answer2" == "yes" ]; then
         ssh-keygen
-        ssh-copy-id -p 41999 server@noftp.ddns.net
+        ssh-copy-id -p 41999 $Server_user@$Server_dns
+        sudo ssh-keygen
+        sudo ssh-copy-id -p 41999 $Server_user@$Server_dns
     elif [ "$answer2" == "no" ]; then 
-        ssh-copy-id -p 41999 server@noftp.ddns.net
+        ssh-copy-id -p 41999 $Server_user@$Server_dns
+        sudo ssh-copy-id -p 41999 $Server_user@$Server_dns
     fi
 elif [ "$answer" == "fck_u" ]; then
     echo "  Any: I hate you and fuck u too"
