0675c9dd21fbe9478e1078227b8b0ae684e0cfa3
Senaxius <alexander.minor004@gmail.com>
2021-10-08 14:49:14 +0200

    config file and identity file

--- fast-install.sh
+++ fast-install.sh
@@ -1,34 +1,66 @@
 #!/bin/bash
-Server_user=$(sed -n 5p ./anyshell)
-Server_user=$(echo $Server_user | sed -n 's/^.*=//p')
-Server_dns=$(sed -n 2p ./anyshell)
-Server_dns=$(echo $Server_dns | sed -n 's/^.*=//p')
+# Server_user=$(sed -n 5p ./anyshell)
+# Server_user=$(echo $Server_user | sed -n 's/^.*=//p')
+# Server_dns=$(sed -n 2p ./anyshell)
+# Server_dns=$(echo $Server_dns | sed -n 's/^.*=//p')
 
 cat ./.anyshell/asci.txt
-echo -e "\n\nWelcome to the official anyshell Installer! :)"
+echo -e "\n\nWelcome to the official anyshell Installer! :)\n\n"
+
+if [ -f ./.anyshell/config ]; then
+    echo "Config file found! Create new one? (yes | no):"
+    read i
+    if [ "$i" == "yes" ]; then
+        config=1
+    else
+        config=0
+    fi
+else
+    echo "No config file found, creating new one..."
+    config=1
+fi
+
+if [ "$config" == "1" ]; then
+    read -p "Server dns: " dns
+    read -p "Server local-IP: " local_IP
+    read -p "Server User: " User
+    echo "$dns" > ./.anyshell/config
+    echo "$local_IP" >> ./.anyshell/config
+    echo "$User" >> ./.anyshell/config
+fi
+
+Server_user=$(sed -n 3p ./.anyshell/config)
+Server_dns=$(sed -n 1p ./.anyshell/config)
+
+echo $Server_user
+echo $Server_dns
 
 DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 ln -s $DIR/.anyshell $HOME
 sudo ln -s $DIR/.anyshell /root
 sudo timedatectl set-timezone "Europe/Berlin"
 
-echo -e "You need to move your public key to the server, if you prefer to do it by yourself, type no, else typ yes"
-read -p "(no | yes): " answer
-if [ "$answer" == "yes" ]; then
-    echo "Do you want me to generate a key?"
-    read -p "(yes | no): " answer2
-    if [ "$answer2" == "yes" ]; then
-        ssh-keygen
-        ssh-copy-id -p 41999 $Server_user@$Server_dns
-        sudo ssh-keygen
-        sudo ssh-copy-id -p 41999 $Server_user@$Server_dns
-    elif [ "$answer2" == "no" ]; then 
-        ssh-copy-id -p 41999 $Server_user@$Server_dns
-        sudo ssh-copy-id -p 41999 $Server_user@$Server_dns
-    fi
-elif [ "$answer" == "no" ]; then
-    echo "ok"
+if [ ! -f ~/.ssh/anyshell-key ]; then
+    echo -e "\nNo key found, creating ssh-key\n"
+    ssh-keygen -f "$HOME/.ssh/anyshell-key" -P "" 
+    echo -e "\nSending to server...\n"
+    ssh-copy-id -i ~/.ssh/anyshell-key.pub -p 41999 $Server_user@$Server_dns 
+else
+    echo -e "\nKey found, sending to server...\n"
+    ssh-copy-id -i ~/.ssh/anyshell-key.pub -p 41999 $Server_user@$Server_dns
 fi
+
+if [ ! -f /root/.ssh/anyshell-key ]; then
+    echo -e "\nNo root key found, creating ssh-key\n"
+    sudo ssh-keygen -f "/root/.ssh/anyshell-key" -P "" 
+    echo -e "\nSending to server...\n"
+    sudo ssh-copy-id -i /root/.ssh/anyshell-key.pub -p 41999 $Server_user@$Server_dns
+else
+    echo -e "\n Root-Key found, sending to server...\n"
+    sudo ssh-copy-id -i /root/.ssh/anyshell-key.pub -p 41999 $Server_user@$Server_dns 
+fi
+
+exit 1
 
 sudo ln -s $DIR/anyshell /usr/bin
 sudo ln -s ~/.anyshell/anyshell-deamon.service /etc/systemd/system/
